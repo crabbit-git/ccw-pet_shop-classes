387b15c2a50c73a6c7d2f488a60cf77c49bf7709
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-03-22 13:18:12 +0000

    All tests pass except final integration

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -2,4 +2,28 @@ class PetShop:
     def __init__(self, name, stock, cash):
         self.name = name
         self.stock = stock
-        self.cash = cash
+        self.total_cash = cash
+        self.pets_sold = 0
+    
+    def total_cash(self):
+        return self.total_cash
+    
+    def stock_count(self):
+        return len(self.stock)
+    
+    def increase_pets_sold(self):
+        self.pets_sold += 1
+    
+    def increase_total_cash(self, transact):
+        self.total_cash += transact
+    
+    def add_pet(self, pet):
+        self.stock.append(pet)
+    
+    def remove_pet(self, pet):
+        self.stock.remove(pet)
+    
+    def find_pet_by_name(self, pet_name):
+        for pet in self.stock:
+            if pet.name == pet_name:
+                return pet
