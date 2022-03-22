6f770a834873acbcb5db6ef0003c91e5b78864a0
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-03-22 13:00:51 +0000

    Customer tests all pass

--- src/customer.py
+++ src/customer.py
@@ -2,3 +2,16 @@ class Customer:
     def __init__(self, name, cash):
         self.name = name
         self.cash = cash
+        self.pets = []
+    
+    def pet_count(self):
+        return len(self.pets)
+    
+    def add_pet(self, pet):
+        self.pets.append(pet)
+    
+    def get_total_value_of_pets(self):
+        total_cost = 0
+        for pet in self.pets:
+            total_cost += pet.price
+        return total_cost
